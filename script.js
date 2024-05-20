3ee01282e8bbafa6ab4d63b3e6aeef55d5607be5
unknown <malibu1106@gmail.com>
2024-05-20 17:47:04 +0200

    Numbers / hints OK - Need CSS + Input / Check player code

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let codeToCrack = getRandomNumber();
 let formattedCode = codeToCrack.toString().padStart(3, '0');
-document.getElementById('debug').innerHTML = "Code to Crack : " + formattedCode + "<br>";
+let codeToCrackBoxes = document.getElementsByClassName('box');
+
+function displayCodeToCrack() {
+    for (let i = 0; i < 3; i++) {
+        codeToCrackBoxes[i].innerHTML = formattedCode.charAt(i);
+    }
+}
+displayCodeToCrack();
 
 function getRandomNumber() {
     let isValid;
@@ -17,7 +24,7 @@ function getRandomNumber() {
     return code;
 }
 
-function generateHintOne() {
+function generateHintOne() { // Un correct et bien placé, mais autres !=
     let hintOne;
     let matchCount;
     let isValid;
@@ -39,7 +46,7 @@ function generateHintOne() {
 }
 generateHintOne();
 
-function generateHintTwo() {
+function generateHintTwo() { // Rien est correct
     let hintTwo;
     let matchCount;
     do {
@@ -51,7 +58,137 @@ function generateHintTwo() {
             }
         }
     } while (matchCount > 0);
-    document.getElementById('debug').innerHTML += "Rien n'est correct: " + hintTwo;
+    document.getElementById('debug').innerHTML += "Rien n'est correct: " + hintTwo + "<br>";
     return hintTwo;
 }
 generateHintTwo();
+
+
+function generateHintThree() {
+    let hintThree;
+    let matchCount;
+    let isValid = false;
+
+    do {
+        hintThree = getRandomNumber().toString().padStart(3, '0');
+        matchCount = 0;
+
+        for (let i = 0; i < 3; i++) {
+            if (formattedCode.includes(hintThree[i]) && formattedCode[i] !== hintThree[i]) {
+                matchCount++;
+            }
+        }
+
+
+        if (matchCount === 1) {
+
+            isValid = true;
+            for (let i = 0; i < 3; i++) {
+                if (formattedCode.includes(hintThree[i]) && (formattedCode[i] !== hintThree[i])) {
+
+                    continue;
+                }
+                if (formattedCode.includes(hintThree[i]) && (formattedCode[i] === hintThree[i])) {
+
+                    isValid = false;
+                    break;
+                }
+                if (!formattedCode.includes(hintThree[i])) {
+
+                    continue;
+                }
+            }
+        }
+    } while (!isValid);
+
+    document.getElementById('debug').innerHTML += "Un chiffre est correct mais mal placé : <span id='ht'>" + hintThree + "</span><br>";
+    return hintThree;
+}
+generateHintThree();
+
+
+function generateHintFour() {
+    let hintFour;
+    let matchCount;
+    let isValid = false;
+
+    do {
+        hintFour = getRandomNumber().toString().padStart(3, '0');
+        matchCount = 0;
+
+        // Pour chaque chiffre dans hintTwo
+        for (let i = 0; i < 3; i++) {
+            if (formattedCode.includes(hintFour[i]) && formattedCode[i] !== hintFour[i]) {
+                matchCount++;
+            }
+        }
+
+        // Vérifiez que deux chiffres sont corrects mais mal placés
+        if (matchCount === 2) {
+            // Vérifiez que le troisième chiffre n'est pas présent dans formattedCode
+            isValid = true;
+            for (let i = 0; i < 3; i++) {
+                if (formattedCode.includes(hintFour[i]) && (formattedCode[i] !== hintFour[i])) {
+                    // Si le chiffre est trouvé mais mal placé, passez
+                    continue;
+                }
+                if (formattedCode.includes(hintFour[i]) && (formattedCode[i] === hintFour[i])) {
+                    // Si un chiffre est à la même position, c'est invalide
+                    isValid = false;
+                    break;
+                }
+                if (!formattedCode.includes(hintFour[i])) {
+                    // Si un chiffre n'est pas trouvé du tout, c'est valide
+                    continue;
+                }
+            }
+        }
+    } while (!isValid);
+
+    document.getElementById('debug').innerHTML += "Deux chiffres sont corrects mais mal placés : " + hintFour + "<br>";
+    return hintFour;
+}
+generateHintFour();
+
+
+function generateHintFive() {
+    let hintFive;
+    let matchCount;
+    let isValid = false;
+
+    do {
+        hintFive = getRandomNumber().toString().padStart(3, '0');
+        matchCount = 0;
+
+        for (let i = 0; i < 3; i++) {
+            if (formattedCode.includes(hintFive[i]) && formattedCode[i] !== hintFive[i]) {
+                matchCount++;
+            }
+        }
+
+
+        if (matchCount === 1 && hintFive !== document.getElementById('ht').innerHTML) {
+
+            isValid = true;
+            for (let i = 0; i < 3; i++) {
+                if (formattedCode.includes(hintFive[i]) && (formattedCode[i] !== hintFive[i])) {
+
+                    continue;
+                }
+                if (formattedCode.includes(hintFive[i]) && (formattedCode[i] === hintFive[i])) {
+
+                    isValid = false;
+                    break;
+                }
+                if (!formattedCode.includes(hintFive[i])) {
+
+                    continue;
+                }
+            }
+        }
+    } while (!isValid);
+
+    document.getElementById('debug').innerHTML += "Un chiffre est correct mais mal placé : " + hintFive + "<br>";
+    return hintFive;
+}
+generateHintFive();
